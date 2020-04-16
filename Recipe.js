4dd8985737baa5ec704338ff980123db380dad6f
gg <labankiplagat81@gmail.com>
2020-04-16 17:54:09 +0300

    Created recipe component.

---
+++ Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Recipe = () => {
+    return(
+        <div>
+            <h1> Title</h1>
+            <p> Description </p>
+            <img src="" alt=""></img>
+        </div>
+    )
+}
+
+
+
+export default Recipe;e
--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ function App(){
 
   const APP_ID = "96d96e26"
   const APP_KEY = "d92d714f7c665ef92c6e260e31db72bf"
+  
 
+  const [recipes, setRecipes] = useState([]);
   
 
   useEffect( () => {
+
+    getRecipes();
     
   }, []);
 
@@ -22,7 +26,8 @@ function App(){
        
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await  response.json();
-    console.log(data);
+    console.log(data.hits);
+    setRecipes(data.hits)
   }
   return(
 
