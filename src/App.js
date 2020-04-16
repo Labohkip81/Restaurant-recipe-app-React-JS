88c0c8f26701469369d65e59364e2e2de860b714
gg <labankiplagat81@gmail.com>
2020-04-16 16:52:43 +0300

    added APi keys and made use of the useEffect.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
+
+
+
+function App(){
+
+  const APP_ID = "96d96e26"
+  const APP_KEY = "d92d714f7c665ef92c6e260e31db72bf"
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  })
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form className="search-form">
+         <input className="search-bar" type="text" />
+         <button className="search-button" >Search</button>
+       </form>
+       <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
     </div>
+
   );
+  
 }
+
+
 
 export default App;
